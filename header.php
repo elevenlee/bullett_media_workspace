cf95fa156b72a1223234e71c3523653107cd66ce
Eleven Lee <sl3268@nyu.edu>
2013-03-18 10:33:45 -0400

    add zoom on products

---
+++ header.php
@@ -0,0 +1,114 @@
+<!doctype html>
+<!--[if lt IE 7]><html class="no-js ie6 oldie" lang="en"><![endif]-->
+<!--[if IE 7]><html class="no-js ie7 oldie" lang="en"><![endif]-->
+<!--[if IE 8]><html class="no-js ie8 oldie" lang="en"><![endif]-->
+<!--[if IE 9]><html class="no-js ie9 oldie" lang="en"><![endif]-->
+<!--[if gt IE 8]><!--><html class="no-js" lang="en"> <!--<![endif]-->
+<head>
+<!-- Never gonna give you up, never gonna let you down, never gonna run around and desert you. -->
+	<!--<?php wp_title(''); ?>-->
+	<meta charset="<?php bloginfo('charset'); ?>">
+	<title><?php bloginfo('name'); ?></title>
+	<meta name="author" content="Bullett Media">
+	<meta name="title" content="<?php wp_title("//"); ?>"  property="og:title">
+	<meta name="viewport" content="width=device-width">
+	<link rel="shortcut icon" href="<?php bloginfo('template_directory'); ?>/favicon.ico">
+	<?php $attachments = get_children( array(
+		'post_parent'    => get_the_ID(),
+		'post_type'      => 'attachment',
+		'numberposts'    => 1,
+		'post_status'    => 'inherit',
+		'post_mime_type' => 'image',
+		'order'          => 'ASC',
+		'orderby'        => 'menu_order ASC'
+	));
+	foreach ( $attachments as $attachment_id => $attachment ) {
+		$the_fb_src = wp_get_attachment_image_src( $attachment_id );
+		echo '<meta name="image" property="og:image" content="' . $the_fb_src[0] . '" />';
+	}
+	?>
+	<meta name="news_keywords" content="<?php
+		$tags = wp_get_post_tags( $post->ID );
+		$tagline = '';
+		foreach( $tags as $tag ):
+			$tagline .= $tag->name;
+			$tagline .= ', ';
+		endforeach; 
+		$tagline = substr( $tagline, 0, -2 );
+		echo( $tagline ); ?>">
+	<link rel="stylesheet" href="<?php bloginfo('template_directory') ?>/css/style-min-3.0.css">
+	<link rel="stylesheet" type="text/css" href="<?php bloginfo('template_directory') ?>/css/jquery.jqzoom.css">
+	<!--[if lt IE 9]><script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
+	<?php if ( 'slideshow' == get_post_type() ) { ?>
+	<link rel="stylesheet" type="text/css" href="<?php bloginfo('template_directory') ?>/css/lightview/lightview.css"/>
+	<?php } //end if(slideshow)
+	?>
+	
+<!--<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>-->
+<script>window.jQuery || document.write('<script src="<?php bloginfo('template_url'); ?>/scripts/jquery-1.7.2.min.js">x3C/script>')</script>
+<script src="<?php bloginfo('template_directory'); ?>/scripts/jquery-1.6.js"></script>
+<script src="<?php bloginfo('template_directory'); ?>/scripts/jquery.jqzoom-core.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.8.16/jquery-ui.min.js"></script>
+<script src="http://cdn.jquerytools.org/1.2.6/all/jquery.tools.min.js"></script>
+<script src="<?php bloginfo('template_directory'); ?>/scripts/main-raw.js"></script>
+<script src="http://s7.addthis.com/js/250/addthis_widget.js#pubid=ra-4d66e78a3e962981"></script>
+<script src="http://platform.tumblr.com/v1/share.js"></script>
+
+<?php if ( 'slideshow' == get_post_type() ) { ?>
+<!--[if lt IE 9]><script type="text/javascript" src="<?php bloginfo('template_directory') ?>/scripts/lightview/excanvas.js"></script><![endif]-->
+<script type="text/javascript" src="<?php bloginfo('template_directory') ?>/scripts/lightview/spinners.min.js"></script>
+<script type="text/javascript" src="<?php bloginfo('template_directory') ?>/scripts/lightview/lightview.js"></script>
+<?php } //end if(slideshow)
+?>
+
+	
+	<?php wp_head(); ?>
+</head>
+<body class="standard editorial">
+<div id="page-container">
+	<header id="header-container">
+		<?php
+// ---------------------------------------		
+		$empty_cart_msg = "&nbsp;";
+		echo do_shortcode('[shopping_cart empty_msg="'.$empty_cart_msg.'"]'); // modified in cart66/models/Cart66ShortcodeManager.php
+// ---------------------------------------		
+		?>
+
+			<h1><a id="header-logo" href="<?php bloginfo('url'); ?>"><img src="http://shop.bullettmedia.com/wp-content/uploads/2013/01/shop-logo.png" alt="The Bullett Shop" /></a></h1>
+			
+			<div id="top-ad-container">
+				<a href="http://shop.bullettmedia.com/products/magazine-subscription/"><img src="http://bullett.wpengine.netdna-cdn.com/wp-content/uploads/2012/12/imgad-3.jpeg"></a>
+			</div>
+			<div class="clear"></div><!-- added a clear, by FLAT -->
+			<nav>
+				<?php
+				wp_nav_menu(array(
+					'theme_location' => 'header-menu',
+					'container' => false,
+					'menu_class' => 'menu',
+					'menu_id' => 'main-navigation',
+					'items_wrap' => '<ul id="%1$s">%3$s</ul>'
+				));
+				?>
+			</nav>
+<!--! end of nav #main-navigation -->
+			<div id="header-social-nav">
+				<h1>FOLLOW US ON</h1>
+				<ul>
+					<li id="fb-btn"><a href="http://www.facebook.com/pages/Bullett-Magazine/143235499055215" title="facebook">facebook</a></li>
+					<li id="twitter-btn"><a href="http://twitter.com/#!/BULLETTMedia" title="twitter">twitter</a></li>
+					<li id="tumblr-btn"><a href="http://bullettmedia.tumblr.com/" title="tumblr">tumblr</a></li>
+					<li id="pinterest-btn"><a href="http://pinterest.com/bullettmedia/" title="pinterest">pinterest</a></li>
+					<li id="rss-btn"><a href="feed://www.bullettmagazine.com/rss" title="rss">rss</a></li>
+				</ul>
+				<div class="clear"></div><!-- added a clear, by FLAT -->
+			</div>
+<!--! end of #header-social-nav -->
+			<form method="get" id="header-search-form" action="http://bullettmedia.com">
+				<input type="text" class="expand" name="s" id="s" value="SEARCH" onFocus="if (this.value == 'SEARCH') {this.value = '';}" onBlur="if (this.value == '') {this.value = 'SEARCH';}">
+				<input type="submit" name="submit" value="SUBMIT">
+			</form>
+<!--! end of #header-search-form -->
+			<div class="clear"></div>
+	</header>
+<!--! end of #header-container -->
---
+++ single-products.php
@@ -0,0 +1,221 @@
+<?php
+/*
+Template Name: Product Detail / Default
+*/
+# -------------------------------------------------------------------------------------- #
+# -------------------------------------------------------------------------------------- #
+get_header();
+# -------------------------------------------------------------------------------------- #
+# -------------------------------------------------------------------------------------- #
+$related_terms = wp_get_post_terms($post->ID,'products-categories');
+$related_args = array(
+	'post_type'		=> array('products'),
+	'orderby'		=> 'menu_order',
+	'order'			=> 'ASC',
+	'posts_per_page'	=> 6,
+	'tax_query'		=> array(
+		array(
+			'taxonomy' => 'products-categories',
+			'field' => 'slug',
+			'terms' => $related_terms[0]->slug
+		)
+	)
+);
+$related_prods = get_posts($related_args);
+?>
+	<div id="fb-root"></div>
+	<script>(function(d, s, id) { var js, fjs = d.getElementsByTagName(s)[0]; if (d.getElementById(id)) return; js = d.createElement(s); js.id = id; js.src = "//connect.facebook.net/en_US/all.js#xfbml=1"; fjs.parentNode.insertBefore(js, fjs); }(document, 'script', 'facebook-jssdk'));</script>
+	<script>$(document).ready(function() {$('.jqzoom').jqzoom({zoomType: 'innerzoom', lens: true, preloadImages: false, alwaysOn: false, title: false, showEffect: 'fadein', hideEffect: 'fadeout', fadeinSpeed: 'slow', fadeoutSpeed: 'slow', }); });</script>
+	<div id="main-container" class="PRODUCT-DETAIL" role="main" data-script="ProductArticle">
+		<?php if ( have_posts() ): while ( have_posts() ) : the_post();
+			# -------------------------------------------------------------------------- #
+			$post_id		= get_the_ID();
+			$post_title		= basename(get_permalink());
+			$post_content	= get_the_content();
+			$post_madeby	= get_post_meta($post_id,'Manufacturer', true);
+			$add_to_cart	= null;
+			preg_match_all("/\[add_to_cart (.*)\]/i", $post_content, $add_to_cart);
+			preg_match_all("/<img (.*)>/", $post->post_content, $product_images);
+			
+			$thumb_ID = get_post_thumbnail_id( $post->ID );
+			$thumb_hover_ID = null;
+			if (class_exists('MultiPostThumbnails') && MultiPostThumbnails::get_post_thumbnail_id('products', 'featured-image-roll', get_the_ID())) : 
+				$thumb_hover_ID = MultiPostThumbnails::get_post_thumbnail_id('products', 'featured-image-roll', get_the_ID());
+			endif;
+			
+			$attachments = get_children(array('post_parent'=>get_the_ID(),'post_status'=>'inherit', 'post_type'=>'attachment', 'post_mime_type'=>'image', 'exclude'=>array($thumb_hover_ID), 'order'=>'ASC', 'posts_per_page'=>-1));
+			# -------------------------------------------------------------------------- #
+			?>
+			<div id="left-column" style="margin: 0px 0px; position: absolute; top: 0; left: 0; float: none;">
+				<article class="post-column fixed" style="position: static; top: 0; padding: 23px 0 0 0;">
+					<div class="post-scroll-pane">
+						<h1><?php the_title(); ?></h1>
+						<?php if( strlen($post_madeby) ) { ?>
+						<h3 class="category-slug">BY <?php echo $post_madeby; ?></h3>
+						<?php } ?>
+						<div class="product-price">
+							<?php echo do_shortcode($add_to_cart[0][0]); ?>
+						</div>
+						<div class="experpt">
+							<p><?php $straight_content = preg_replace("/\[.+?\]/", "", $post_content); echo $straight_content ?></p>
+						</div>
+						<div class="post-share-container" style="position: relative; top: 0px; left: 0px;">
+							<ul>
+								<li><fb:like layout="box_count" style="width:48px;"></fb:like></li>
+								<li><a class="addthis_button_tweet" tw:count="vertical"></a></li>
+								<li class="pin"><a class="addthis_button_pinterest" pi:pinit:url="http://pinterest.com/bullettmedia/"></a></li>
+							</ul>
+							<ul>
+								<li><a class="fb-share-btn" href="http://api.addthis.com/oexchange/0.8/forward/facebook/offer?url=<?php echo get_permalink();?>" rel="nofollow" target="_blank">Share</a></li>
+								<li><a class="tumblr-share-btn" href="http://www.tumblr.com/share/link?url=<?php echo urlencode(get_permalink()) ?>&amp;name=<?php echo urlencode(get_the_title()) ?>" title="Share on Tumblr">Share on Tumblr</a></li>
+								<li><a class="email-counter" href="http://api.addthis.com/oexchange/0.8/forward/email/offer?url=<?php echo get_permalink();?>" rel="nofollow" target="_blank">EMAIL</a></li>
+							</ul>
+							<!--
+							<ul>
+								<li class="image-btn comments"><a id="comments-counter" href="#">COMMENTS</a></li>
+							</ul>
+							-->
+						</div>
+					</div>
+				</article>
+			</div>
+			<!--! end of #left-column -->
+			
+			<div id="content-column" style="margin: 30px 0px 100px 352px; width: 581px;">
+				<div class="inner" style="padding: 0 30px;">
+					<?php
+					/*
+					preg_match_all("/wp-image-(d+)/", $post->post_content, $matches);
+					unset($matches[0]);
+					$attachments = get_children( array('post_parent' => get_the_ID(), 'post_status' => 'inherit', 'post_type' => 'attachment', 'post_mime_type' => 'image', 'order' => 'ASC', 'orderby' => 'menu_order ID', 'posts_per_page' => -1, 'post__not_in' => $matches[1]) );
+					if($attachments):
+						foreach($attachments as $attachment): if(strstr($post->post_content, "wp-image-" . $attachment->ID)) continue;
+						
+						endforeach;
+					endif;
+					*/
+					if($attachments):
+					?>
+					<div id="post-slideshow" style="height: auto; overflow: inherit; width: 480px;">
+						<!--<div class="post-slideshow-prev">&lt;</div>-->
+						<div class="slideshow-wrapper" style="display: block;">
+							<?php
+							$slide_count = 0;
+							foreach($attachments as $attachment): ?>
+							<div class="post-slide <?php if ($slide_count==0) { echo 'current'; } ?>" id="post-slide-<?php echo $attachment->ID; ?>" style="width: 480px; display: <?php if ($slide_count<=0) { echo 'block'; } else { echo 'none'; } ?>;">
+								<span style="height: auto; width: 480px;"><?php
+								//	echo wp_get_attachment_image($attachment->ID, $size='384high', $icon=0, $attr=array("width"=>"480"));
+									$slide	 = wp_get_attachment_image_src($attachment->ID, $size='384high', $icon=0);
+									$slide_w = $slide[1];
+									$slide_h = $slide[2]; 
+									if($slide_w <> 480) { 
+										$percentage	 = 480/$slide_w;
+										$slide_h = $slide_h * $percentage;
+									}
+									$slide_w = 480;
+									$slide_h = ceil($slide_h);
+									echo '<a href="'.$slide[0].'" class="jqzoom">';
+									echo '<img src="'.$slide[0].'" width="480" height="'.$slide_h.'" class="attachment-384high" />';
+									echo '</a>';
+									if ($attachment->post_excerpt != "null") :
+										echo "<p>".$attachment->post_excerpt."</p>";
+									endif;
+									?>
+								</span>
+							</div>
+							<?php
+							$slide_count++;
+							endforeach; ?>
+							<div class="clear"></div>
+						</div>
+						<!--<div class="post-slideshow-next">&gt;</div>-->
+					</div>
+					<!--! end of #post-slideshow -->
+					
+					<?php endif; ?>
+					
+					<div class="RELATED-ITEMS">
+							<h2>More like this item</h2>
+							<ul class="related-items">
+							<?php foreach( $related_prods as $rp ) : ?>
+								<li><a href="<?php echo get_permalink($rp->ID); ?>">
+									<?php the_image("thumbnail", "related-thumnail", $rp->ID); ?>
+									<p><?php echo get_the_title($rp->ID); ?></p>
+								</a></li>
+							<?php endforeach; ?>
+							</ul>
+					</div>
+					
+				</div>	
+			</div>
+			<!--! end of #content-column -->
+			
+			<div class="right-wrapper" style="float: none; width: auto;">
+				<div id="right-column" style="position: relative; top: 0px; left: 0px; min-height: 100%; width: 150px;">
+					
+					<div id="product-thumbs-container">
+						<div id="related-carousel-container">
+							<div class="caroufredsel_wrapper" style="float: none; position: relative;">
+								<ul style="float: none; position: relative;">
+								<?php
+								if($attachments):
+									foreach($attachments as $attachment):
+										echo "<li>";
+										echo "<a href='javascript:void(0);' class='slide-toggle' data='post-slide-$attachment->ID'>";
+										echo wp_get_attachment_image($attachment->ID, 'thumbnail');
+										echo "</a>";
+										echo "</li>";
+									endforeach;
+								endif;
+								?>
+								</ul>
+							</div>
+						</div>
+						<nav>
+							<div class="prev-btn" style="display: block; ">Up</div>					
+							<div class="next-btn" style="display: block; ">Down</div>					
+						</nav>
+					</div>
+					
+					
+					<script language="javascript">
+					$('a.slide-toggle').click( function(e) {
+						var slide_current = $('.post-slide.current');
+						var slide_select = "#" + $(this).attr('data');
+						if( !$(slide_select).hasClass('current') ) {
+							$(slide_current).removeClass('current').hide();
+							$(slide_select).show().addClass('current');
+						}
+					});
+					$(document).ready(function(){
+						var ddfix = $('#product-thumbs-container ul').clone().addClass('ddfix').hide().appendTo('#footer-container');
+					});
+					$('#options_1').change(function(eventobj){
+						var num = $('#options_1 option[value="' + $('#options_1').val() + '"]').index();
+						var slide_current = $('.post-slide.current');
+						slide_select = "#" + $('.ddfix .slide-toggle').eq(num).attr('data');
+	
+						if( !$(slide_select).hasClass('current') ) {
+							$(slide_current).removeClass('current').hide(); 
+							$(slide_select).show().addClass('current');
+						}
+					});
+					</script>
+					
+				</div>
+				<!--! end of #right-column -->
+			</div>
+			<!--! end of .right-wrapper -->
+			
+		<?php endwhile; endif; ?>
+	</div>
+	<!--! end of #main-container -->
+	
+</div>
+<!--! end of #page-container -->
+
+<div id="post-fullscreen-gallery">
+	<a id="fullscreen-close-btn" class="post-close-btn" href="#">X</a>
+</div>
+<!--! end of #post-fullscreen-gallery -->	
+<?php get_footer(); ?>
